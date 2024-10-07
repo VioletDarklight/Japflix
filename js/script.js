230290b8db86d1d6cff06ef2f4a3fefba5bd2d74
Alison Gonzalez <168144637+VioletDarklight@users.noreply.github.com>
2024-10-06 23:48:12 -0300

    Odern del codigo y comentarios

--- js/script.js
+++ js/script.js
@@ -1,81 +1,9 @@
-let movieList = []
-function showMovieInfo(movieId) {
-  let body = document.getElementsByTagName("body")[0];
-
-  let movie = movieList.find((movie) => movie.id == movieId);
-
-  document.querySelectorAll(".offcanvas").forEach((offcanvas) => {
-    offcanvas.remove();
-  });
-
-  body.innerHTML += `
-    <div id="offcanvasTop"
-      class="offcanvas offcanvas-top show border-danger text-bg-dark" tabindex="-1" aria-labelledby="offcanvasTopLabel">
-      <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasTopLabel">${movie.title}</h5>
-      <button type="button" class="btn-close text-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-      ${movie.overview}
-    </div>
-    <hr>
-    <div class="offcanvas-body card-body">
-      <div class="row">
-        <div class="col">
-          ${movie.genres.map((genre) => genre.name).join(" - ")}
-        </div>
-        <div class="col text-end">
-          <div class="dropdown position-absolute end-0 me-3">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              More
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end ">
-              <li class="dropdown-item">
-                <div class="row flex-nowrap">
-                  <div class="col text-start">Year:</div>
-                  <div class="col text-end">${movie.release_date}</div>
-                </div>
-              </li>
-              <li class="dropdown-item">
-                <div class="row flex-nowrap">
-                  <div class="col text-start">Runtime:</div>
-                  <div class="col text-end">${movie.runtime}</div>
-                </div>
-              </li>
-              <li class="dropdown-item">
-                <div class="row flex-nowrap">
-                  <div class="col text-start">Budget:</div>
-                  <div class="col text-end">${movie.budget}</div>
-                </div>
-              </li>
-              <li class="dropdown-item">
-                <div class="row flex-nowrap">
-                  <div class="col text-start">Revenue:</div>
-                  <div class="col text-end">${movie.revenue}</div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  `;
-
-  // Initialize the offcanvas component
-  let offcanvasElement = document.getElementById("offcanvasTop");
-  let offcanvas = new bootstrap.Offcanvas(offcanvasElement);
-  offcanvas.show();
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   //1. Cuando la página cargue, deberá traer el listado de información sobre películas disponible
   let movies_api = "https://japceibal.github.io/japflix_api/movies-data.json"; //Link del API que continene la lista de peliculas
   let container_list = document.getElementById("lista"); //Lugar donde se mostrara lo que viene del link de arriba, es decir la lista de peliculas
   let btnsearch = document.getElementById("btnBuscar");
   let searchtext = document.getElementById("inputBuscar");
-  let movieinfo = document.getElementById("movie-info");
-  let movieinfodropdown = document.getElementById("dropdownbtn");
 
   // Función para convertir la votación numérica (sobre 10) en estrellas (sobre 5)
   function getStars(stars) {
@@ -145,3 +73,77 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("Error en la carga de películas");
     });
 });
+
+let movieList = [] //array que contiene la lista de peliculas buscadas
+
+//Funcion para mostrar la informacion de las peliculas al hace click en ellas.
+function showMovieInfo(movieId) {
+  let body = document.getElementById("body");
+
+  let movie = movieList.find((movie) => movie.id == movieId);
+
+  // Buscamos todos los elementos con la clase offcanvas y los eliminamos para arreglar el bug de que se abren varios offcanvas en el DOM al
+  // hacer click en las tarjetas de las películas
+  document.querySelectorAll(".offcanvas").forEach((offcanvas) => {
+    offcanvas.remove();
+  });
+
+  body.innerHTML += `
+    <div id="offcanvasTop"
+      class="offcanvas offcanvas-top show border-danger text-bg-dark" tabindex="-1" aria-labelledby="offcanvasTopLabel">
+      <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasTopLabel">${movie.title}</h5>
+      <button type="button" class="btn-close text-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      ${movie.overview}
+    </div>
+    <hr>
+    <div class="offcanvas-body card-body">
+      <div class="row">
+        <div class="col">
+          ${movie.genres.map((genre) => genre.name).join(" - ")}
+        </div>
+        <div class="col text-end">
+          <div class="dropdown position-absolute end-0 me-3">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              More
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end ">
+              <li class="dropdown-item">
+                <div class="row flex-nowrap">
+                  <div class="col text-start">Year:</div>
+                  <div class="col text-end">${movie.release_date}</div>
+                </div>
+              </li>
+              <li class="dropdown-item">
+                <div class="row flex-nowrap">
+                  <div class="col text-start">Runtime:</div>
+                  <div class="col text-end">${movie.runtime}</div>
+                </div>
+              </li>
+              <li class="dropdown-item">
+                <div class="row flex-nowrap">
+                  <div class="col text-start">Budget:</div>
+                  <div class="col text-end">${movie.budget}</div>
+                </div>
+              </li>
+              <li class="dropdown-item">
+                <div class="row flex-nowrap">
+                  <div class="col text-start">Revenue:</div>
+                  <div class="col text-end">${movie.revenue}</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  // Activa el componente offcanvas de boostrap
+  let offcanvasElement = document.getElementById("offcanvasTop");
+  let offcanvas = new bootstrap.Offcanvas(offcanvasElement);
+  offcanvas.show();
+}
